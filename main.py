5c24b5c90114ca37e47a31f89efd247b02c2c13d
Diana Carolina Serrano <dianacaroserranot@gmail.com>
2025-05-26 09:18:32 -0500

    Inicio de proyecto del grupo Lunes

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+#importanciones
+
+
+print("Hola este es nuestro primer proyeto como equipo")
+
+#Alejo, Haz una lista de los paises que te gustaria conocer
+
+paises = ["Colombia", "peru"]
+
+#Alan: importa un random y con un choice escoge un pais
 

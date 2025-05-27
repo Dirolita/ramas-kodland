5108d1d4e06991d098fb128899a11e681b22c671
alejo diaz <alejodiaztopia@gmail.com>
2025-05-27 10:37:37 -0600

    modificacion en la nueva rama

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ print("Hola este es nuestro primer proyeto como equipo")
 
 #Alejo, Haz una lista de los paises que te gustaria conocer
 
-paises = ["Colombia", "peru"]
+paises = ["Colombia","Peru","Chile","Argentina","Ecuador","Peru","Uruguay","Bolivia","Republica Dominicana","Puerto Rico","Costa Rica","Panama"]
 
 #Alan: importa un random y con un choice escoge un pais
 
